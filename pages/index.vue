36c1a474ce89f59ba6ee8fd8ff9dec559d91af87
Lecrut <filip.lecrut12@gmail.com>
2024-04-05 15:14:06 +0200

    generate first basic layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,27 @@
 </script>
 
 <template>
-  Witamy na stonie randkowej
+  <v-card
+      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-15 px-4"
+      elevation="4"
+      max-width="1100"
+      min-height="200"
+      image="/landingOne.jpeg"
+      rounded
+  >
+    Witamy na stronie randkowej
+  </v-card>
+
+  <v-card
+      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
+      elevation="4"
+      max-width="1100"
+      min-height="200"
+      image="/landingTwo.jpeg"
+      rounded
+  >
+    Zobacz jak to działa
+  </v-card>
 </template>
 
 <style scoped>
