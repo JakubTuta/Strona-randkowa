8c7145470c970bacbbeccaac75964bb55f964fa7
Lecrut <filip.lecrut12@gmail.com>
2024-04-08 22:32:17 +0200

    add navbar

---
+++ composables/navBars/navBarGuest.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <v-app-bar
+    prominent
+    class="px-2"
+  >
+    <v-btn variant="text" color="default" to="/">
+      Randki+
+    </v-btn>
+
+    <v-spacer />
+
+    <div class="hidden-sm-and-down">
+      <v-btn variant="text" color="default" to="/auth/login">
+        Logowanie
+      </v-btn>
+
+      <v-btn variant="text" color="default" icon="mdi-theme-light-dark" @click="toggleTheme" />
+    </div>
+
+    <div class="hidden-md-and-up">
+      <v-btn class="rounded-xl mr-2" color="default">
+        <v-icon icon="mdi-menu" />
+        <v-menu activator="parent">
+          <v-list class="justify-center">
+            <v-list-item to="/auth/login">
+              <v-list-item-title>Logowanie</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-
+import NavBarGuest from '~/composables/navBars/navBarGuest.vue'
 </script>
 
 <template>
+  <NavBarGuest />
   <v-card
-      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-15"
-      elevation="4"
-      max-width="1100"
-      min-height="200"
-      rounded
+    class="d-flex align-center justify-center flex-wrap text-center mx-auto my-15"
+    elevation="4"
+    max-width="1100"
+    min-height="200"
+    rounded
   >
     <v-img
-        src="/public/landingOne.jpeg"
-        max-height="200"
-        cover
-        gradient="to bottom, rgba(0,0,0,.2), rgba(0,0,0,.7)"
+      src="/public/landingOne.jpeg"
+      max-height="200"
+      cover
+      gradient="to bottom, rgba(0,0,0,.2), rgba(0,0,0,.7)"
     >
       <div class="d-flex flex-column align-center justify-center h-100 mx-2">
         <div class="text-h4">
@@ -29,21 +30,20 @@
         </v-btn>
       </div>
     </v-img>
-
   </v-card>
 
   <v-card
-      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-15"
-      elevation="4"
-      max-width="1100"
-      min-height="200"
-      rounded
+    class="d-flex align-center justify-center flex-wrap text-center mx-auto my-15"
+    elevation="4"
+    max-width="1100"
+    min-height="200"
+    rounded
   >
     <v-img
-        src="/public/landingTwo.jpeg"
-        max-height="200"
-        cover
-        gradient="to bottom, rgba(0,0,0,.2), rgba(0,0,0,.7)"
+      src="/public/landingTwo.jpeg"
+      max-height="200"
+      cover
+      gradient="to bottom, rgba(0,0,0,.2), rgba(0,0,0,.7)"
     >
       <div class="d-flex flex-column align-center justify-center h-100 mx-2">
         <div class="text-h4">
@@ -58,10 +58,5 @@
         </v-btn>
       </div>
     </v-img>
-
   </v-card>
 </template>
-
-<style scoped>
-
-</style>
