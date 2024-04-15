9bdaff4542fdb3584fe471e029f0a903b2d3bf60
Jakub Tutka <jakubtutka02@gmail.com>
2024-04-15 15:43:01 +0200

    basic response

--- python/server.py
+++ python/server.py
@@ -1,20 +1,24 @@
-# import flask
-
-# app = flask.Flask(__name__)
-
-
-# @app.route("/")
-# def index():
-#     pass
-
-
-# if __name__ == "__main__":
-#     app.run(port=2137)
-
 import database.database_functions as db_functions
+import flask
 
-# db_functions.get_all_users()
-user_ref = db_functions.get_reference_from_id("pierwszy")
-user = db_functions.get_user(user_ref)
+app = flask.Flask(__name__)
 
-print(user)
+
+@app.route("/matches_api", methods=["POST"])
+def matches():
+    try:
+        data = flask.request.get_json(force=True)
+
+        user_reference = data["reference"]
+    except Exception as e:
+        print(e)
+        return flask.jsonify({"error": str(e)}), 400
+
+    users = db_functions.get_other_users(user_reference)
+    users_references = [user["reference"] for user in users]
+
+    return flask.jsonify(users_references), 200
+
+
+if __name__ == "__main__":
+    app.run(port=2137)
