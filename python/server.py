3ad429ff2de398ed053237ab42ddf085fb8118bd
Jakub Tutka <jakubtutka02@gmail.com>
2024-04-21 23:34:43 +0200

    hotfix

--- python/server.py
+++ python/server.py
@@ -12,13 +12,13 @@ def matches():
         user_reference = data["reference"]
     except Exception as e:
         print(e)
-        return flask.jsonify({"error": str(e)}), 400
+        return flask.Response(flask.jsonify({"error": str(e)}), 400)
 
     users = db_functions.get_other_users(user_reference)
     users_references = [user["reference"] for user in users]
 
-    return flask.jsonify(users_references), 200
+    return flask.Response(flask.jsonify(users_references), 200)
 
 
 if __name__ == "__main__":
-    app.run(port=2137)
+    app.run(port=2137, debug=True)
