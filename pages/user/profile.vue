3306251cc734f25a7da49c0fa9875eb57e2847f6
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-05-01 12:51:26 +0200

    basic profile view

---
+++ pages/user/profile.vue
@@ -0,0 +1,67 @@
+<script>
+import NavBarUser from '~/components/navBarUser.vue'
+import ProfileTab from '~/components/user/profileTab.vue'
+</script>
+
+<template>
+  <NavBarUser />
+  <v-row justify="center">
+    <v-col cols="12" sm="12" , md="6">
+      <v-sheet
+        class="d-flex align-center flex-wrap text-center mx-auto my-10 px-4" elevation="4" max-width="1100"
+        rounded
+      >
+        <v-row justify="center">
+          <v-col cols="12" sm="12" md="12">
+            <div class="d-flex flex-column align-center justify-center h-100 mx-2 pa-6">
+              <v-img
+                class="mx-auto my-5 elevation-5" rounded="xl" :width="300" aspect-ratio="4/3" cover
+                src="/testPerson3.jpg"
+              />
+            </div>
+          </v-col>
+        </v-row>
+      </v-sheet>
+    </v-col>
+
+    <v-col cols="12" sm="12" , md="6">
+      <v-sheet
+        class="d-flex align-center flex-wrap text-center mx-auto my-10 px-4" elevation="4" max-width="1100"
+        rounded
+      >
+        <v-col cols="12" sm="12" md="12">
+          <v-row>
+            <v-col sm="12" md="6">
+              <div class="text-h4 pa-3 align-center">
+                IMIĘ NAZWISKO, 22
+              </div>
+            </v-col>
+            <v-col sm="12" md="6">
+              <div class="text-h6 my-2 pa-3">
+                @anne123
+              </div>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="12" sm="12">
+              <v-text-field label="Opis" readonly />
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field label="Data urodzenia" readonly />
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field label="Płeć" readonly />
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field label="Wydział" readonly />
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field label="Kierunek" readonly />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
