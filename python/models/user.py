c1e2d2e4aeb4ab212a42207d4e66bd8afc637b32
Jakub Tutka <jakubtutka02@gmail.com>
2024-05-01 11:21:16 +0200

    hotfix python user model

--- python/models/user.py
+++ python/models/user.py
@@ -4,7 +4,7 @@ import typing
 from google.cloud import firestore
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(kw_only=True)
 class UserModel:
     user_name: str = ""
     photos: typing.List[str] = dataclasses.field(
@@ -29,11 +29,6 @@ class UserModel:
     reference: firestore.DocumentReference = dataclasses.field(
         default=None, compare=False, hash=False, repr=False
     )
-
-    def __init__(self, data, reference):
-        for key, value in data.items():
-            setattr(self, key, value)
-        setattr(self, "reference", reference)
 
     def to_map(self):
         data = dataclasses.asdict(self)
