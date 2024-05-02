a106671ba98996d1206dca195f9ea71da6ddef3c
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-05-02 13:11:28 +0200

    showing hobbies to profile

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,6 +1,5 @@
 <script>
 import NavBarUser from '~/components/navBarUser.vue'
-import ProfileTab from '~/components/user/profileTab.vue'
 </script>
 
 <template>
@@ -8,18 +7,22 @@ import ProfileTab from '~/components/user/profileTab.vue'
   <v-row justify="center">
     <v-col cols="12" sm="12" , md="6">
       <v-sheet
-        class="d-flex align-center flex-wrap text-center mx-auto my-10 px-4" elevation="4" max-width="1100"
-        rounded
+        class="d-flex align-center flex-wrap text-center mx-auto my-10 px-4" elevation="4" max-height="1100"
+        max-width="1100" rounded
       >
         <v-row justify="center">
-          <v-col cols="12" sm="12" md="12">
-            <div class="d-flex flex-column align-center justify-center h-100 mx-2 pa-6">
+          <v-col cols="12" sm="12" md="6">
+            <div>
               <v-img
                 class="mx-auto my-5 elevation-5" rounded="xl" :width="300" aspect-ratio="4/3" cover
                 src="/testPerson3.jpg"
               />
             </div>
           </v-col>
+        </v-row>
+
+        <v-row>
+          reszta zdjęć
         </v-row>
       </v-sheet>
     </v-col>
@@ -31,33 +34,72 @@ import ProfileTab from '~/components/user/profileTab.vue'
       >
         <v-col cols="12" sm="12" md="12">
           <v-row>
-            <v-card class="mx-auto">
-              <v-card-title class="text-h4">
+            <div class="mx-auto">
+              <v-card-title class="text-h4 flex-wrap">
                 IMIĘ NAZWISKO, 22
               </v-card-title>
               <v-card-subtitle>
                 @anne123
               </v-card-subtitle>
-            </v-card>
+            </div>
           </v-row>
 
           <v-row>
             <v-col cols="12" md="12" sm="12">
-              <v-text-field label="Opis" readonly />
+              <v-text-field :label="$t('profile.description')" readonly />
             </v-col>
             <v-col cols="12" md="6" sm="12">
-              <v-text-field label="Data urodzenia" readonly />
+              <v-text-field :label="$t('profile.dateBirth')" readonly />
             </v-col>
             <v-col cols="12" md="6" sm="12">
-              <v-text-field label="Płeć" readonly />
+              <v-text-field :label="$t('profile.gender')" readonly />
             </v-col>
             <v-col cols="12" md="6" sm="12">
-              <v-text-field label="Wydział" readonly />
+              <v-text-field :label="$t('profile.faculty')" readonly />
             </v-col>
             <v-col cols="12" md="6" sm="12">
-              <v-text-field label="Kierunek" readonly />
+              <v-text-field :label="$t('profile.fieldOfStudy')" readonly />
             </v-col>
           </v-row>
+
+          <v-card-actions class="justify-end">
+            <v-btn>
+              Edytuj dane
+            </v-btn>
+          </v-card-actions>
+
+          <v-row>
+            <div class="mx-auto">
+              <v-card-title class="text-h4">
+                Twoje preferencje i zainteresowania
+              </v-card-title>
+            </div>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field :label="$t('profile.lookingFor')" readonly />
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field :label="$t('profile.prefferedGender')" readonly />
+            </v-col>
+            <v-col cols="12" md="12" sm="12">
+              <v-chip-group>
+                <v-chip
+                  v-for="element in ['Silownia', 'Piłka', 'Książki', 'Praca', 'IT', 'Basen', 'Koszenie trawy']"
+                  :key="element" size="large"
+                >
+                  {{ element }}
+                </v-chip>
+              </v-chip-group>
+            </v-col>
+          </v-row>
+
+          <v-card-actions class="justify-end">
+            <v-btn @click="print()">
+              Edytuj preferencje
+            </v-btn>
+          </v-card-actions>
         </v-col>
       </v-sheet>
     </v-col>
