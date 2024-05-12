3fedc94bbec91b7cebb4884f7c2e56a24f5ceb76
Jakub Tutka <jakubtutka02@gmail.com>
2024-05-12 23:52:17 +0200

    cleanup

--- python/RecommendationAlgorithm.py
+++ python/RecommendationAlgorithm.py
@@ -3,32 +3,34 @@ import typing
 import database.database_functions as db_functions
 from models.user import UserModel
 
-# zakładając że elo mieści się od 500 do 2000
-ELO_SCORE = {
-    "MIN_ORIGINAL_SCORE": 500,
-    "MAX_ORIGINAL_SCORE": 2000,
-    "MIN_NORMALIZED_SCORE": 0.0,
-    "MAX_NORMALIZED_SCORE": 1.0,
-}
-
-# zakładając że rating może być od 0 do 5
-RATING_SCORE = {
-    "MIN_ORIGINAL_SCORE": 0.0,
-    "MAX_ORIGINAL_SCORE": 5.0,
-    "MIN_NORMALIZED_SCORE": 0.0,
-    "MAX_NORMALIZED_SCORE": 1.0,
-}
-
-# punkty z każdej kategorii mają się mieścić od 0 do 1
-SCORING = {
-    "IS_PREFERRED_GENDER": 1.0,
-    "IS_PHOTO": 1.0,
-    "IS_DESCRIPTION": 1.0,
-    "IS_LIKE": 1.0,
-}
-
 
 class RecommendationAlgorithm:
+    # elo mieści się od 500 do 2000
+    __ELO_SCORE = {
+        "MIN_ORIGINAL_SCORE": 500,
+        "MAX_ORIGINAL_SCORE": 2000,
+        "MIN_NORMALIZED_SCORE": 0.0,
+        "MAX_NORMALIZED_SCORE": 1.0,
+    }
+
+    # rating może być od 0 do 5
+    __RATING_SCORE = {
+        "MIN_ORIGINAL_SCORE": 0.0,
+        "MAX_ORIGINAL_SCORE": 5.0,
+        "MIN_NORMALIZED_SCORE": 0.0,
+        "MAX_NORMALIZED_SCORE": 1.0,
+    }
+
+    # punkty z każdej kategorii mają się mieścić od 0 do 1
+    __SCORING = {
+        "IS_PREFERRED_GENDER": 1.0,
+        "IS_PHOTO": 1.0,
+        "IS_DESCRIPTION": 1.0,
+        "IS_LIKE": 1.0,
+    }
+
+    __MAX_PHOTOS = 9
+
     @staticmethod
     def score_one_user(user: UserModel, other: UserModel) -> float:
         other_score = 0
@@ -65,9 +67,9 @@ class RecommendationAlgorithm:
 
         if user.lookingFor == "relationship":
             if user.preferredGender == other.gender:
-                score += SCORING["IS_PREFERRED_GENDER"]
+                score += RecommendationAlgorithm.__SCORING["IS_PREFERRED_GENDER"]
             else:
-                score -= SCORING["IS_PREFERRED_GENDER"]
+                score -= RecommendationAlgorithm.__SCORING["IS_PREFERRED_GENDER"]
 
         return score
 
@@ -76,10 +78,16 @@ class RecommendationAlgorithm:
         user_elo = user.elo
 
         normalized_elo = (
-            (user_elo - ELO_SCORE["MIN_ORIGINAL_SCORE"])
-            / (ELO_SCORE["MAX_ORIGINAL_SCORE"] - ELO_SCORE["MIN_ORIGINAL_SCORE"])
-        ) * (ELO_SCORE["MAX_NORMALIZED_SCORE"] - ELO_SCORE["MIN_NORMALIZED_SCORE"])
-        +ELO_SCORE["MIN_NORMALIZED_SCORE"]
+            (user_elo - RecommendationAlgorithm.__ELO_SCORE["MIN_ORIGINAL_SCORE"])
+            / (
+                RecommendationAlgorithm.__ELO_SCORE["MAX_ORIGINAL_SCORE"]
+                - RecommendationAlgorithm.__ELO_SCORE["MIN_ORIGINAL_SCORE"]
+            )
+        ) * (
+            RecommendationAlgorithm.__ELO_SCORE["MAX_NORMALIZED_SCORE"]
+            - RecommendationAlgorithm.__ELO_SCORE["MIN_NORMALIZED_SCORE"]
+        )
+        +RecommendationAlgorithm.__ELO_SCORE["MIN_NORMALIZED_SCORE"]
 
         return normalized_elo
 
@@ -88,24 +96,32 @@ class RecommendationAlgorithm:
         user_score = user.score
 
         normalized_score = (
-            (user_score - RATING_SCORE["MIN_ORIGINAL_SCORE"])
-            / (RATING_SCORE["MAX_ORIGINAL_SCORE"] - RATING_SCORE["MIN_ORIGINAL_SCORE"])
+            (user_score - RecommendationAlgorithm.__RATING_SCORE["MIN_ORIGINAL_SCORE"])
+            / (
+                RecommendationAlgorithm.__RATING_SCORE["MAX_ORIGINAL_SCORE"]
+                - RecommendationAlgorithm.__RATING_SCORE["MIN_ORIGINAL_SCORE"]
+            )
         ) * (
-            RATING_SCORE["MAX_NORMALIZED_SCORE"] - RATING_SCORE["MIN_NORMALIZED_SCORE"]
+            RecommendationAlgorithm.__RATING_SCORE["MAX_NORMALIZED_SCORE"]
+            - RecommendationAlgorithm.__RATING_SCORE["MIN_NORMALIZED_SCORE"]
         )
-        +RATING_SCORE["MIN_NORMALIZED_SCORE"]
+        +RecommendationAlgorithm.__RATING_SCORE["MIN_NORMALIZED_SCORE"]
 
         return normalized_score
 
     @staticmethod
     def __check_if_is_photo(user: UserModel) -> float:
-        score = (len(user.photos) > 0) * SCORING["IS_PHOTO"]
+        score = (
+            len(user.photos) / RecommendationAlgorithm.__MAX_PHOTOS
+        ) * RecommendationAlgorithm.__SCORING["IS_PHOTO"]
 
         return score
 
     @staticmethod
     def __check_if_is_description(user: UserModel) -> float:
-        score = (len(user.description) > 0) * SCORING["IS_DESCRIPTION"]
+        score = (len(user.description) > 0) * RecommendationAlgorithm.__SCORING[
+            "IS_DESCRIPTION"
+        ]
 
         return score
 
@@ -113,7 +129,7 @@ class RecommendationAlgorithm:
     def __check_if_likes(user: UserModel, other: UserModel) -> float:
         score = (
             db_functions.check_if_other_likes_user(user.reference, other.reference)
-            * SCORING["IS_LIKE"]
+            * RecommendationAlgorithm.__SCORING["IS_LIKE"]
         )
 
         return score
