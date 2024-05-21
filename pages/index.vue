38717f24ef2de4c92f3a0bce46963dd8786d607b
Jakub Tutka <jakubtutka02@gmail.com>
2024-05-21 20:19:16 +0200

    test :D

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
           :width="466"
           aspect-ratio="4/3"
           cover
-          src="/LandingOne.jpg"
+          src="/landingOne.jpg"
         />
       </v-col>
       <v-col cols="12" sm="12" md="6">
