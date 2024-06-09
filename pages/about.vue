6e20776884e7797c5d1e58ec25e864f1a4792f46
Lecrut <filip.lecrut12@gmail.com>
2024-06-09 12:46:06 +0200

    text on about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  const { t } = useI18n()
 </script>
 
 <template>
@@ -8,37 +9,34 @@
       gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)"
       class="mt-15 text-center"
   >
-    <div class="d-flex flex-column align-center justify-center h-100 mx-2">
-      <span class="text-h4 font-weight-bold text-white">
-        Witamy na stronie randkowej
+    <div class="d-flex flex-column align-center justify-center h-100 mx-4">
+      <span class="text-h3 font-weight-bold text-white">
+        {{ t('about.seeWhatGoingOn') }}
       </span>
 
       <p class="mt-2 text-white">
-        Aplikacja zaprojektowana specjalnie do tego żeby ją usunąć. Zaintrygowało?
+        {{ t('about.shortDescription') }}
       </p>
 
     </div>
   </v-img>
 
   <v-card
-      class="mx-10 my-10 px-4"
+      class="mx-10 mt-10 px-4"
       elevation="0"
       min-height="400"
       rounded="xl"
       color="transparent"
   >
-    <div class="d-flex flex-column align-center justify-center h-100 mx-2 my-15 text-center">
-      <div class="text-h4">
-        Zobacz jak to działa
+    <div class="d-flex flex-column align-center justify-center h-100 mx-2 mt-15 text-center">
+      <div class="text-h4 mb-5 font-weight-bold">
+        {{ t('about.relationships') }}
       </div>
 
       <p class="mt-3">
-        Przygotowaliśmy specjalnie dla ciebie krótki opis jak działa nasza aplikacja.
+        {{ t('about.relationshipsDescription') }}
       </p>
 
-      <v-btn class="my-4" to="/about" variant="elevated" color="primary">
-        Przejdź
-      </v-btn>
     </div>
   </v-card>
 
@@ -48,38 +46,57 @@
       gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)"
       class="text-center"
   >
-    <div class="d-flex flex-column align-center justify-center h-100 mx-2">
+    <div class="d-flex flex-column align-center justify-center h-100 mx-4">
       <span class="text-h4 font-weight-bold text-white">
-        Witamy na stronie randkowej
+        {{ t('about.chats') }}
       </span>
 
       <p class="mt-2 text-white">
-        Aplikacja zaprojektowana specjalnie do tego żeby ją usunąć. Zaintrygowało?
+        {{ t('about.chatsDescription') }}
       </p>
 
     </div>
   </v-img>
 
   <v-card
-      class="mx-10 my-10 px-4"
+      class="mx-10 mt-10 px-4"
       elevation="0"
       min-height="400"
       rounded="xl"
       color="transparent"
   >
     <div class="d-flex flex-column align-center justify-center h-100 mx-2 my-15 text-center">
-      <div class="text-h4">
-        Zobacz jak to działa
+      <div class="text-h4 mb-5 font-weight-bold">
+        {{ t('about.events') }}
       </div>
 
-      <p class="mt-3">
-        Przygotowaliśmy specjalnie dla ciebie krótki opis jak działa nasza aplikacja.
+      <p class="my-3">
+        {{ t('about.eventsDescription') }}
       </p>
 
-      <v-btn class="my-4" to="/about" variant="elevated" color="primary">
-        Przejdź
+
+      <v-btn class="mt-4" to="/events" variant="elevated" color="primary">
+        {{ t('about.goToEvents') }}
       </v-btn>
     </div>
   </v-card>
+
+  <v-img
+      src="/aboutTwo.jpg"
+      cover
+      gradient="to bottom, rgba(0,0,0,.25), rgba(0,0,0,.7)"
+      class="text-center"
+  >
+    <div class="d-flex flex-column align-center justify-center h-100 mx-2">
+      <span class="text-h4 font-weight-bold text-white">
+        {{ t('about.whatsNext') }}
+      </span>
+
+      <v-btn class="mt-4" to="/auth/register" variant="elevated" color="primary">
+        {{ t('about.whatsNextDescription') }}
+      </v-btn>
+
+    </div>
+  </v-img>
 
 </template>
