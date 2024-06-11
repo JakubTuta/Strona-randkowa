5454674b7129050b295f4e36664f2ec141b8b606
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-06-11 22:22:03 +0200

    displaying uncoming  events on main page

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,7 +1,32 @@
 <script lang="ts" setup>
+import type { EventModel } from '~/models/event'
+import { useEventsStore } from '~/stores/eventsStore'
+import ShowEventDialog from '~/components/user/events/showEventDialog.vue'
+
 definePageMeta({
   layout: 'user',
   middleware: ['auth'],
+})
+
+const { t } = useI18n()
+const eventStore = useEventsStore()
+const { events } = storeToRefs(eventStore)
+
+const showForm = ref(false)
+const showedEvent: Ref<EventModel | null> = ref(null)
+
+function showEventForm(event: EventModel) {
+  showForm.value = true
+  showedEvent.value = event
+}
+
+function closeForm() {
+  showedEvent.value = null
+  showForm.value = false
+}
+
+onMounted(async () => {
+  await eventStore.getFutureEvents()
 })
 </script>
 
@@ -16,33 +41,28 @@ definePageMeta({
     </v-row>
     <v-row justify="center">
       <v-col cols="12" md="8">
-        <v-carousel class="rounded-xl" cycle :interval="5000">
-          <v-carousel-item
-            src="/testEvent.jpg"
-            cover
-          >
-            <v-btn variant="elevated" size="large" color="secondary">
-              Dołącz teraz!
-            </v-btn>
-          </v-carousel-item>
+        <v-carousel v-if="events.length" class="rounded-xl" cycle :interval="5000">
+          <template v-for="(item, index) in events" :key="index" :item="item">
+            <v-carousel-item
+              src="/testEvent.jpg"
+              cover
+              class="text-center"
+            >
+              <v-row justify="center">
+                <v-col>
+                  <div class="d-flex fill-height justify-center align-center text-h4 py-2 bg-grey-darken-4">
+                    {{ item.name }}
+                  </div>
 
-          <v-carousel-item
-            src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-            cover
-          >
-            <v-btn variant="elevated" size="large" color="secondary">
-              Dołącz teraz!
-            </v-btn>
-          </v-carousel-item>
-
-          <v-carousel-item
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            cover
-          >
-            <v-btn variant="elevated" size="large" color="secondary">
-              Dołącz teraz!
-            </v-btn>
-          </v-carousel-item>
+                  <div class="d-flex fill-height justify-center align-center">
+                    <v-btn variant="elevated" size="large" color="secondary" @click="showEventForm(item)">
+                      {{ t('events.index.showEvent') }}
+                    </v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-carousel-item>
+          </template>
         </v-carousel>
       </v-col>
     </v-row>
@@ -65,6 +85,8 @@ definePageMeta({
       </v-col>
     </v-row>
   </v-container>
+
+  <ShowEventDialog :event="showedEvent" :is-show="showForm" @on-close="closeForm" />
 </template>
 
 <style scoped>
