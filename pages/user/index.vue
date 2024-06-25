be5247bf70b5f7bf0eb34055ff5c9a8b658a5821
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-06-25 20:22:04 +0200

    added date to events carusels

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -43,6 +43,16 @@ onMounted(async () => {
   await eventStore.getFutureEvents()
   await appStore.fetchLikedProfiles(userData.value?.matches || [])
 })
+
+function convertDate(date) {
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+
+  return `${day}.${month}.${year} ${hours}:${minutes}`
+}
 </script>
 
 <template>
@@ -65,8 +75,11 @@ onMounted(async () => {
             >
               <v-row justify="center">
                 <v-col>
-                  <div class="d-flex fill-height justify-center align-center text-h4 py-2 bg-grey-darken-4">
+                  <div class="d-flex justify-center align-center text-h4 py-2 bg-grey-darken-4">
                     {{ item.name }}
+                  </div>
+                  <div class="d-flex justify-center align-center text-h4 py-2 bg-grey-darken-4">
+                    {{ convertDate(item.startDate) }}
                   </div>
 
                   <div class="d-flex fill-height justify-center align-center">
